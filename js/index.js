d5ebd45e4629cbc36dbe1a1298a160473c44c535
Lili Molloy <liliana.molloy@gmail.com>
2018-11-23 14:15:43 +1100

    fixed the post method. some css on the form

--- js/index.js
+++ js/index.js
@@ -72,3 +72,69 @@ function createBook(book) {
     return li 
 }
 
+//get the form 
+//add event listener on submit
+// post a book
+// refresh fetchBooks()
+
+const bookForm = document.querySelector('#bookForm')
+
+bookForm.addEventListener('submit', function(e) {
+
+    postBook(e).then(book => {addBooks(book)})
+})
+
+function postBook(e) {
+    e.preventDefault()
+    const title = e.target.title.value
+    const isbn = e.target.isbn.value
+    const description = e.target.description.value
+    const price = e.target.price.value
+    const author = e.target.author.value
+
+    const book = {
+        title, description, isbn, price, author
+    }
+
+    const url = 'http://localhost:3000/books'
+
+    const options = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(book) // turns from js object to JSON
+    }
+    e.target.reset()
+
+    return fetch(url, options).then( res => {return res.json()}) // promise object
+}
+
+
+
+//Authors
+const authorList = document.querySelector('.authors')
+
+function  createAuthor(authorData) {
+    const author = document.createElement('li')
+    const link = document.createElement('a')
+    link.href = '#' // `authors/${id}`
+    const {first_name: firstName, last_name: lastName} = authorData; //deconstructive method
+    link.innerText = `${firstName} ${lastName}`
+    // or link.innerText = `${authorData.firstName} ${authorData.lastName}`
+    author.appendChild(link)
+    return author // promise
+}
+
+function fetchAuthor() {
+    const url = 'http://localhost:3000/authors'
+    const data = fetch(url).then(res => res.json())
+    return data 
+}
+
+function addAuthor(author) {
+    authorList.appendChild(createAuthor(author))
+}
+
+fetchAuthor()
+        .then(authors => authors.forEach(addAuthor))
